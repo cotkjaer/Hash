74d6509b4ff200fa9bc6d399b8110779dc70c1bc
christian otkjær <christian.otkjaer@gmail.com>
2016-02-18 16:23:29 +0100

    Initial Commit

---
+++ Hash/Hash.h
@@ -0,0 +1,19 @@
+//
+//  Hash.h
+//  Hash
+//
+//  Created by Christian Otkjær on 18/02/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Hash.
+FOUNDATION_EXPORT double HashVersionNumber;
+
+//! Project version string for Hash.
+FOUNDATION_EXPORT const unsigned char HashVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hash/PublicHeader.h>
+
+
